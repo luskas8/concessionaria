c9d63dac156bab9b3f599c6d871b8cd8a12d0ae1
Luskas8 <gnomo1049@gmail.com>
2020-12-03 17:38:03 -0300

    Fix: isChooseMistaken

--- utils.c
+++ utils.c
@@ -16,9 +16,9 @@
 bool isThisChooseMistaken(char header[], char idOwner[], int * typedId) {
   printf("%s", header);
   printf("Nao deseja estar aqui? Entre com '-1' para voltar ao menu principal.\n\n");
-  printf("Informe o codigo do %s", idOwner);
+  printf("Informe o codigo do %s: ", idOwner);
   read_int(typedId);
-  return (*typedId == -1 ? false : true);
+  return (*typedId == -1);
 }
 
 
